fe96a726026ec05f65eca5ab9f8c89597756313f
Cobalt6700 <44681021+Cobalt6700@users.noreply.github.com>
2025-03-12 17:51:13 +0000

    Create CrConfig-JMRI-DCC_3P_3S_1Ser.cmd

---
+++ utility/CrConfig-JMRI-DCC_3P_3S_1Ser.cmd
@@ -0,0 +1,42 @@
+﻿@echo off
+rem Bitte keine Zeilen lösche, nur den Wert hinter dem = anpassen
+rem Hinter den Werten dürfen keine Leerzeichen stehen
+rem Modell-Name des Decoders in JMRI ( Buchstaben,Zahlen und _ ):
+set model=3_Point_3-2Aspect_1Ser
+rem Interface: DCC oder LocoNet. LocoNet ist standard
+set interface=DCC
+rem Konfiguration des Decoders (Funktionen):
+rem Es dürfen keine Lücken entstehen. Nicht verwendete Funktionen bleiben am Ende frei
+set INITYP1=FSERVO
+set INITYP2=FSERVO
+set INITYP3=FSERVO
+set INITYP4=FSIGNAL2
+set INITYP5=FSIGNAL2
+set INITYP6=FSIGNAL2
+set INITYP7=FSERIAL
+set INITYP8=
+set INITYP9=
+set INITYP10=
+set INITYP11=
+set INITYP12=
+rem ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+rem ------------ Ende des Benutzeranpassbaren Bereiches ---------------------------------
+
+echo #define _MODEL %model%> %model%.txt
+echo #define _INTERFACE %interface%>> %model%.txt
+echo #define INITYP1 %INITYP1%>> %model%.txt
+echo #define INITYP2 %INITYP2%>>  %model%.txt
+echo #define INITYP3 %INITYP3%>>  %model%.txt
+echo #define INITYP4 %INITYP4%>>  %model%.txt
+echo #define INITYP5 %INITYP5%>>  %model%.txt
+echo #define INITYP6 %INITYP6%>>  %model%.txt
+echo #define INITYP7 %INITYP7%>>  %model%.txt
+echo #define INITYP8 %INITYP8%>>  %model%.txt
+echo #define INITYP9 %INITYP9%>>  %model%.txt
+echo #define INITYP10 %INITYP10%>>  %model%.txt
+echo #define INITYP11 %INITYP11%>>  %model%.txt
+echo #define INITYP12 %INITYP12%>>  %model%.txt
+gpp -s "\"" +n  --include %model%.txt  -I .\src  -o Public_Domain_%interface%_%model%.xml src\Public_Domain_GPP-Generic.gpp
+rem pause
+del %model%.txt
+pause
